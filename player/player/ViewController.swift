77eabbaac7db327ad6c193d357c80be92bf285c4
강재원 <pzzaq@naver.com>
2019-04-19 16:32:35 +0900

    commit

---
+++ player/player/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  player
+//
+//  Created by kang on 18/04/2019.
+//  Copyright © 2019 kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
